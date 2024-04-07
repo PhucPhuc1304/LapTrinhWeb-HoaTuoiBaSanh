ea061dd56b17afc9b1a73b196c69510af26b4ef2
PhucPhuc1304 <ngohoangphuc.work@gmail.com>
2024-04-07 13:10:07 +0700

    Update 07/04

--- Models/IndexViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HoaTuoiBaSanh_Core6.Models
-{
-    public class IndexViewModel
-    {
-        public List<Product> NewProducts { get; set; }
-        public List<Product> SaleProducts { get; set; }
-    }
-}
