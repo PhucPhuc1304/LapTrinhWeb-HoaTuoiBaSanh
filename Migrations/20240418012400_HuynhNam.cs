8198a0e04150ba5d43bbc798d05b122a6053bd8f
NamHuynh <kimdong240502@gmail.com>
2024-04-18 09:15:06 +0700

    update

---
+++ Migrations/20240418012400_HuynhNam.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CF_HOATUOIBASANH.Migrations
+{
+    public partial class HuynhNam : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- obj/Debug/net8.0/CF_HOATUOIBASANH.AssemblyInfo.cs
+++ obj/Debug/net8.0/CF_HOATUOIBASANH.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CF_HOATUOIBASANH")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a32873fce610bb2114131219b3213df694037f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc8cb268a91a938fa283501facbfa52406c6c70a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CF_HOATUOIBASANH")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CF_HOATUOIBASANH")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
